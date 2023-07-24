13a56bb9f3488f345401c061da6aeba0308290d4
Nikolay Ignatov <ignatov.99@yandex.ru>
2023-07-24 13:25:27 +0300

    Initial Commit

---
+++ Native Avatar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Native Avatar
+//
+//  Created by Николай Игнатов on 24.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
